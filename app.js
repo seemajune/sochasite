5ec62fba8b0695f130f7d254c5d2eb31d61acd9c
seemajune <seemashariat@gmail.com>
2015-11-12 18:17:53 -0500

    updates to connect db to the app

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/sochasite');
+
 var routes = require('./routes/index');
 var contact = require('./routes/contact');
 
@@ -21,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/contact', contact);
