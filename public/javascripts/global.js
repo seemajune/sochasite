d7be0091fd28775feb78bab19017373e466bad73
seemajune <seemashariat@gmail.com>
2015-11-22 12:40:51 -0500

    updates navbar color to match logo and begin sweetalert imp

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,47 +1,47 @@
 var userListData = []; //data to filling in info box
 
 $(document).ready(function() {
-    populateTable(); // Populate the user table on initial page load
-    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo); // username link Click
+    //populateTable(); // Populate the user table on initial page load
+    // $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo); // username link Click
     $('#btnAddUser').on('click', addUser);  // Add User button click
-    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser); // Delete User link click
+    // $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser); // Delete User link click
 });
 
-function populateTable() { //fill table on pg load w/ data
+// function populateTable() { //fill table on pg load w/ updated data
 
-    var tableContent = '';     // Empty content string
+//     var tableContent = '';     // Empty content string
 
-    $.getJSON( '/usercollection', function(data) {     // jQuery AJAX call for JSON
+//     $.getJSON( '/usercollection', function(data) {     // jQuery AJAX call for JSON
 
-      userListData = data; // puts db data into global var
+//       userListData = data; // puts db data into global var
 
-        $.each(data, function(){ // For each item in our JSON, add a table row and cells to the content string
-            tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-            tableContent += '<td>' + this.email + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-            tableContent += '</tr>';
-        });
+//         $.each(data, function(){ // For each item in our JSON, add a table row and cells to the content string
+//             tableContent += '<tr>';
+//             tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+//             tableContent += '<td>' + this.email + '</td>';
+//             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+//             tableContent += '</tr>';
+//         });
 
-        $('#userList table tbody').html(tableContent);  // Inject the whole content string into our existing HTML table
-    });
-};
+//         $('#userList table tbody').html(tableContent);  // Inject the whole content string into our existing HTML table
+//     });
+// };
 
-function showUserInfo(e) { // Show User Info
+// function showUserInfo(e) { // Show User Info
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    var thisUserName = $(this).attr('rel'); // Retrieve username from link rel attribute
+//     var thisUserName = $(this).attr('rel'); // Retrieve username from link rel attribute
 
-    // Get Index of object based on id value
-    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+//     // Get Index of object based on id value
+//     var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
 
-    var thisUserObject = userListData[arrayPosition]; // Get our User Object
+//     var thisUserObject = userListData[arrayPosition]; // Get our User Object
 
-    //Populate Info Box
-    $('#userInfoName').text(thisUserObject.username);
-    $('#userInfoEmail').text(thisUserObject.email);
-};
+//     //Populate Info Box
+//     $('#userInfoName').text(thisUserObject.username);
+//     $('#userInfoEmail').text(thisUserObject.email);
+// };
 
 function addUser(e) { // Add User Info
 
@@ -68,11 +68,9 @@ function addUser(e) { // Add User Info
         }).done(function(response) {  
 
             if (response.msg === '') { // Check for successful (blank) response
-              // ADD SUCCESS MODAL HERE 
+             
               $('#addUser fieldset input').val(''); // Clear the form inputs
-
-              populateTable();  // Update the table if user (seema is logged in)
-              // if not, success modal
+               // ADD SUCCESS MODAL HERE 
             }
             else {
                 alert('Error: ' + response.msg); 
@@ -87,28 +85,27 @@ function addUser(e) { // Add User Info
     }
 };
 
-function deleteUser(e) {
-  e.preventDefault();
+// function deleteUser(e) {
+//   e.preventDefault();
 
-  var confirmation = confirm('Are you sure you want to delete this user?');
+//   var confirmation = confirm('Are you sure you want to delete this user?');
   
-  if (confirmation === true) {
-    $.ajax({
-      type: 'DELETE',
-      url: '/usercollection/deleteuser/' + $(this).attr('rel')
-    }).done(function(response){
-        if (response.msg === '') { 
-        // Check for successful (blank) response
-        }
-        else {
-          alert('Error: ' + response.msg);
-          //ADD ERROR MODAL HERE
-        }
-        populateTable();
-      });
-  }
-  else {
-    return false; //if they said no to the confirmation box
-  }
-};
+//   if (confirmation === true) {
+//     $.ajax({
+//       type: 'DELETE',
+//       url: '/usercollection/deleteuser/' + $(this).attr('rel')
+//     }).done(function(response){
+//         if (response.msg === '') { 
+//          // ADD SUCCESS MODAL HERE 
+//         }
+//         else {
+//           alert('Error: ' + response.msg);
+//           //ADD ERROR MODAL HERE
+//         }
+//       });
+//   }
+//   else {
+//     return false; //if they said no to the confirmation box
+//   }
+// };
 
