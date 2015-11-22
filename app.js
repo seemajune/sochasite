b7c3f0c44dc47531bba44a2586d41c29ab567609
seemajune <seemashariat@gmail.com>
2015-11-22 11:41:53 -0500

    updated design

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var usercollection = require('./routes/usercollection');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Socha' });
 });
 
+
 /* POST to Add User Service */
 router.post('/', function(req, res) {
 
