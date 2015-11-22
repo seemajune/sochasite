3c65e12ffd8b7f73781986986605058e6b4d9ac7
seemajune <seemashariat@gmail.com>
2015-11-22 13:26:56 -0500

    adds error handing to input fields

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,57 +1,34 @@
 var userListData = []; //data to filling in info box
 
 $(document).ready(function() {
-    //populateTable(); // Populate the user table on initial page load
-    // $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo); // username link Click
     $('#btnAddUser').on('click', addUser);  // Add User button click
-    // $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser); // Delete User link click
 });
-
-// function populateTable() { //fill table on pg load w/ updated data
-
-//     var tableContent = '';     // Empty content string
-
-//     $.getJSON( '/usercollection', function(data) {     // jQuery AJAX call for JSON
-
-//       userListData = data; // puts db data into global var
-
-//         $.each(data, function(){ // For each item in our JSON, add a table row and cells to the content string
-//             tableContent += '<tr>';
-//             tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
-//             tableContent += '<td>' + this.email + '</td>';
-//             tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-//             tableContent += '</tr>';
-//         });
-
-//         $('#userList table tbody').html(tableContent);  // Inject the whole content string into our existing HTML table
-//     });
-// };
-
-// function showUserInfo(e) { // Show User Info
-
-//     e.preventDefault();
-
-//     var thisUserName = $(this).attr('rel'); // Retrieve username from link rel attribute
-
-//     // Get Index of object based on id value
-//     var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
-
-//     var thisUserObject = userListData[arrayPosition]; // Get our User Object
-
-//     //Populate Info Box
-//     $('#userInfoName').text(thisUserObject.username);
-//     $('#userInfoEmail').text(thisUserObject.email);
-// };
 
 function addUser(e) { // Add User Info
 
   e.preventDefault();
 
-  var errorCount = 0; // super basic error handling
+  var errorCount = 0, // super basic error handling
+      errorText = '',
+      inputEmail = $('#inputUserEmail'),
+      inputName = $('#inputUserName');
 
-    $('input').each(function(index, val) {
-        if($(this).val() === '') { errorCount++; } // if user didnt put in their name or email, increment errorCount 
-    });
+    if(inputName.val() === '') { 
+      errorCount++; // if user didnt put in their name or email, increment errorCount 
+      errorText += "Please fill in your name.";
+    }
+
+    if(inputEmail.val() === '') { 
+      errorCount++; // if user didnt put in their name or email, increment errorCount 
+      errorText += "\nPlease fill in your email address.";
+    }  
+
+    var re = /\S+@\S+\.\S+/;
+
+    if(!re.test(inputEmail.val())) { // if email contains incorrect chars
+      errorCount++; 
+      errorText += "\nPlease enter a valid email address.";
+    }
 
     if(errorCount === 0) { // Check and make sure errorCount's still at zero
 
@@ -70,7 +47,7 @@ function addUser(e) { // Add User Info
             if (response.msg === '') { // Check for successful (blank) response
              
               $('#addUser fieldset input').val(''); // Clear the form inputs
-               // ADD SUCCESS MODAL HERE 
+               swal({   title: "Hooray!",   text: "We'll email you when Socha is available to download!",   type: "success",   confirmButtonText: "Cool" });
             }
             else {
                 alert('Error: ' + response.msg); 
@@ -79,33 +56,8 @@ function addUser(e) { // Add User Info
         });
     }
     else {
-        alert('Please fill in all fields'); // If errorCount is more than 0, error out 
-        // ADD ERROR MODAL HERE
+        // If errorCount is more than 0, error out 
+        swal({   title: "Error!",   text: errorText,   type: "error",   confirmButtonText: "Cool" });
         return false;
     }
 };
-
-// function deleteUser(e) {
-//   e.preventDefault();
-
-//   var confirmation = confirm('Are you sure you want to delete this user?');
-  
-//   if (confirmation === true) {
-//     $.ajax({
-//       type: 'DELETE',
-//       url: '/usercollection/deleteuser/' + $(this).attr('rel')
-//     }).done(function(response){
-//         if (response.msg === '') { 
-//          // ADD SUCCESS MODAL HERE 
-//         }
-//         else {
-//           alert('Error: ' + response.msg);
-//           //ADD ERROR MODAL HERE
-//         }
-//       });
-//   }
-//   else {
-//     return false; //if they said no to the confirmation box
-//   }
-// };
-
