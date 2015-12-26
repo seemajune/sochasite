d85fba974c3777fd4bc0f0b1f8a8a54e7d9ec0bf
seemajune <seemashariat@gmail.com>
2015-12-26 16:07:57 -0500

    adds account model

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 // passport
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
